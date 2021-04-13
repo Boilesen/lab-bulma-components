1d181e3734709c57e6a44e7939cca0eb46786c1e
Thomas Boilesen <thomasboilesen@gmail.com>
2021-04-13 16:05:19 -0300

    ate iteracao3

---
+++ src/coolButton/CoolButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+function CoolButton({ classButton }) {
+  return <button className={classButton}>Button</button>;
+}
+
+export default CoolButton;
---
+++ src/signup/signup.js
@@ -0,0 +1,27 @@
+import "bulma/css/bulma.css";
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import Button from "./buttons/buttons.js";
+
+const Signup = () => {
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />;
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      ;
+      <FormField label="password" type="password" placeholder="********" />;
+      <Button isSmall isDanger className="is-rounded my-class">
+        {" "}
+        Button 1
+      </Button>
+    </div>
+  );
+};
+
+export default Signup;
